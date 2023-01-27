17648dcabb7e769721778b8d0dbb3a12660a9339
semihdiker <94144467+sammydkr@users.noreply.github.com>
2023-01-27 00:46:57 -0800

    Create Install and Set Up Mongoose myApp.js

---
+++ Install and Set Up Mongoose myApp.js
@@ -0,0 +1,64 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose')
+
+mongoose.connect(
+  process.env.MONGO_URI, 
+  { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+  },
+() => {
+  console.log('DB connection successful')
+}
+);
+
+let Person;
+
+const createAndSavePerson = (done) => {
+  done(null /*, data*/);
+};
+
+const createManyPeople = (arrayOfPeople, done) => {
+  done(null /*, data*/);
+};
+
+const findPeopleByName = (personName, done) => {
+  done(null /*, data*/);
+};
+
+const findOneByFood = (food, done) => {
+  done(null /*, data*/);
+};
+
+const findPersonById = (personId, done) => {
+  done(null /*, data*/);
+};
+
+const findEditThenSave = (personId, done) => {
+  const foodToAdd = "hamburger";
+
+  done(null /*, data*/);
+};
+
+const findAndUpdate = (personName, done) => {
+  const ageToSet = 20;
+
+  done(null /*, data*/);
+};
+
+const removeById = (personId, done) => {
+  done(null /*, data*/);
+};
+
+const removeManyPeople = (done) => {
+  const nameToRemove = "Mary";
+
+  done(null /*, data*/);
+};
+
+const queryChain = (done) => {
+  const foodToSearch = "burrito";
+
+  done(null /*, data*/);
+};
